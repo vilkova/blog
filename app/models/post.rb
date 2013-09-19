2160fe0b68dc734001aa1f73635a71b69ae880b4
Nastya Vilkova <anastaciyavilkova@gmail.com>
2013-09-19 19:02:58 +0300

    added comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   validates_presence_of :title
+  has_many :comments
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Blog::Application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,7 +14,9 @@ Blog::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-resources :posts
+resources :posts do
+  resources :comments, :only => [:create]
+end
   # Example resource route with options:
   #   resources :products do
   #     member do
